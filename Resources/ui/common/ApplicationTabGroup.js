868ce70c6234c1fbd01928d6f0fa3d751a38884c
Lara <martilar@onid.orst.edu>
2013-06-06 14:46:41 -0700

    Revert "Set back to login code"
    This reverts commit c5f5d8cd6240db7ca4d887af57b85441547bb80f.

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -1,18 +1,25 @@
 function ApplicationTabGroup(Window) {
 	//create module instance
 	var self = Ti.UI.createTabGroup();
+	
+	//load component dependencies
+	var Post = require('post');
 
+	
 	//create app tabs
 	var win1 = Titanium.UI.createWindow({
-		title : 'User Authentication Demo',
+		title : 'Post a Job',
 		tabBarHidden : true,
-		url : 'login.js'
 	});
+	
+		//construct UI
+	var post = new Post();
+	win1.add(post);
 	
 	var win2 = new Window(L('settings'));
 
 	var tab1 = Ti.UI.createTab({
-		title : L('home'),
+		title : L('Post a Job'),
 		icon : '/images/KS_nav_ui.png',
 		window : win1
 	});
